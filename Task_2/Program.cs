044af73aa69a641c62f3bebeb35413975c7c3e70
Юлия <YUGG2512@gmail.com>
2023-06-21 20:29:14 +0300

    C_Sharp_Lesson3

---
+++ Task_2/Program.cs
@@ -0,0 +1,25 @@
+﻿//  Напишите программу, которая принимает на вход координаты двух точек 
+// и находит расстояние между ними в 2D пространстве
+// А (3,6); В (2,1) -> 5,09
+// А (7,-5); В (1,-1) -> 7,21
+
+Console.Write("Введите координату x точки A: ");
+int ax = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите координату y точки A: ");
+int ay = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите координату x точки B: ");
+int bx = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите координату x точки B: ");
+int by = int.Parse(Console.ReadLine()!);
+
+double AB = Math.Sqrt(Math.Pow((bx - ax), 2) + Math.Pow((by - ay), 2));
+Console.WriteLine($"Расстояние между точками А и В = {AB:f2}");
+
+
+
+
+
+
---
+++ Task_3/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход число (N)
+// и выдает таблицу квадратов чисел
+
+// 5 -> 1, 4, 9, 16, 25
+// 2 -> 1, 4
+
+Console.Write("Введите число N: ");
+int N = int.Parse(Console.ReadLine()!);
+
+for (int i = 1; i <= N; i++)
+{
+   int sqr = Convert.ToInt32(Math.Pow(i, 2));
+   Console.WriteLine($"{sqr}"); 
+}
