2dec43325bf292da2651046a6d50d3f78fb7e142
Юлия <YUGG2512@gmail.com>
2023-06-21 20:19:35 +0300

    C#_Lesson3

---
+++ Task_1/Program.cs
@@ -0,0 +1,35 @@
+﻿// Напишите программу, которая по заданному номеру честверти
+// показывает диапазон возможных координат точек в этой четверти (х и у).
+
+int number = new Random().Next(1, 5); // номер четверти
+
+switch (number)
+{
+    case 1:
+    {
+        Console.WriteLine($"В чнтверти {number} диапазон возможных координат: x > 0; y > 0");
+        break;
+    }
+    case 2:
+    {
+        Console.WriteLine($"В чнтверти {number} диапазон возможных координат: x < 0; y > 0");
+        break;
+    }
+    case 3:
+    {
+        Console.WriteLine($"В чнтверти {number} диапазон возможных координат: x < 0; y < 0");
+        break;
+    }
+    case 4:
+    {
+        Console.WriteLine($"В чнтверти {number} диапазон возможных координат: x > 0; y < 0");
+        break;
+    }
+    default:
+    {
+        Console.WriteLine("Такой четсверти не существует.");
+        break;
+    }
+}
+
+
